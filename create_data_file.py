d0f4602438acb6cb23a8e5555d9996f534439c17
Majid <majidad09@gmail.com>
2021-10-07 01:58:28 +0330

    train model

---
+++ create_data_file.py
@@ -0,0 +1,43 @@
+import re
+
+files_path = "files/"
+
+""" Read phonemes """
+f = open(files_path + 'phoneme_transcriptions.txt', 'r')
+lines = f.readlines()
+
+# chars = set()
+
+""" spliting test, train anf validation data """
+test_lines = lines[2800:]
+lines = lines[:2800]
+val_lines = lines[:int(len(lines)/10)]
+train_lines = lines[int(len(lines)/10):]
+
+def data_to_dict(line):
+    phonemes = []
+    for line in line:
+        line = line.replace('\n', '')
+        phoneme = line.split('\t')[1]
+        name = line.split('\t')[0]
+
+        phoneme = phoneme.replace('CH', 'c').replace('KH', 'k').replace('SH', 's').replace('SIL', 'i').replace('AH', 'h').replace('ZH', 'z').replace('AA', 'a')  ## replace multi chars by one new char
+        phoneme = re.sub("\[([0-9]+)\]\s*", '', phoneme)
+        # chars.update(set(phoneme.split(' ')))
+        phenome = "/mnt/hdd1/adibian/files/wavs/" + name + ".wav|" + phoneme
+        phonemes.append(phenome)
+    return phonemes
+
+train_data = data_to_dict(train_lines)
+test_data = data_to_dict(test_lines)
+val_data = data_to_dict(val_lines)
+
+with open(files_path + 'text_files/train_data.txt', 'w') as fp:
+    for line in train_data:
+        fp.write(line + '\n')
+with open(files_path + 'text_files/test_data.txt', 'w') as fp:
+    for line in test_data:
+        fp.write(line + '\n')
+with open(files_path + 'text_files/val_data.txt', 'w') as fp:
+    for line in val_data:
+        fp.write(line + '\n')
---
+++ get_results.py
@@ -0,0 +1,69 @@
+import matplotlib
+import matplotlib.pylab as plt
+import IPython.display as ipd
+import numpy as np
+import torch
+import time
+
+
+def plot_data(data, figsize=(16, 4)):
+    fig, axes = plt.subplots(1, len(data), figsize=figsize)
+    for i in range(len(data)):
+        axes[i].imshow(data[i], aspect='auto', origin='lower', 
+                       interpolation='none')
+    plt.savefig("result/plots/output(28000)({}).jpg".format(time.time()))
+    
+
+def load_trained_model(checkpoint_path):
+    hparams = create_hparams()
+    hparams.sampling_rate = 22050
+    model = load_model(hparams)
+    model.load_state_dict(torch.load(checkpoint_path)['state_dict'])
+    _ = model.cuda().eval().half()
+    return model
+   
+def load_vocoder():
+    waveglow = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_waveglow', model_math='fp16', force_download=True)
+    waveglow = waveglow.remove_weightnorm(waveglow)
+    waveglow = waveglow.to('cuda')
+    waveglow.eval().half()
+    for k in waveglow.convinv:
+        k.float()
+    return waveglow
+
+def prepaire_input(text):
+    t2s = text_to_sequence(text, ['english_cleaners'])
+    sequence = np.array(t2s)[None, :]
+    sequence = torch.autograd.Variable(
+        torch.from_numpy(sequence)).cuda().long()
+    return sequence
+
+def get_result_audio(mel_outputs_postnet, denoiser, waveglow):
+    from scipy.io.wavfile import write
+    with torch.no_grad():
+        audio = waveglow.infer(mel_outputs_postnet, sigma=0.666)
+    ipd.Audio(audio[0].data.cpu().numpy(), rate=hparams.sampling_rate)
+    write("result/wavs/audio({}).wav".format(time.time()), rate, audio_numpy)
+
+if __name__ == "__main__":
+    # waveglow = load_vocoder()
+    
+    import sys
+    sys.path.append('tacotron2/')
+    from hparams import create_hparams
+    from model import Tacotron2
+    from layers import TacotronSTFT, STFT
+    from audio_processing import griffin_lim
+    from train import load_model
+    from text import text_to_sequence
+    from waveglow.denoiser import Denoiser
+
+    # denoiser = Denoiser(waveglow).half()
+    model = load_trained_model('tacotron2/outdir/checkpoint_28000')
+    text = 'MODIREKOLLEMIRaSEFARHANGIiSANaYEhEDASTIVAGARDEsGARIYEhOSTaNEYAZDiGOFTi'
+    sequence = prepaire_input(text)
+    mel_outputs, mel_outputs_postnet, _, alignments = model.inference(sequence)
+    plot_data((mel_outputs.float().data.cpu().numpy()[0],
+               mel_outputs_postnet.float().data.cpu().numpy()[0],
+               alignments.float().data.cpu().numpy()[0].T))
+    # get_result_audio(mel_outputs_postnet, denoiser, waveglow)
